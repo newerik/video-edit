d0fc532b40fb4cc5a63affb98cdf10e6cb034a39
Erik Kránicz <newerik@users.noreply.github.com>
2023-05-19 03:22:17 +0200

    Timestamp management complete

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,23 @@ import useRefCallback from './hooks/useRefCallback';
 import useForceUpdate from './hooks/useForceUpdate';
 
 const FPS = 50;
+
+const pad = (input: Big, length = 2) => input.toString().padStart(length, '0');
+
+const toTime = (seconds: number): string => {
+  const bigSec = Big(seconds);
+  const MILLISECONDS = bigSec.mod(1).times(1000);
+  const MS = MILLISECONDS.div(1000);
+  const SS = bigSec.minus(MS).mod(60);
+  const MM = bigSec.minus(MS).minus(SS).div(60).mod(60);
+  const HOURS = bigSec
+    .minus(MS)
+    .minus(SS)
+    .minus(MM.times(60))
+    .div(60 * 60);
+  return `${pad(HOURS)}:${pad(MM)}:${pad(SS)}.${pad(MILLISECONDS, 3)}`;
+};
+
 function App() {
   const forceUpdate = useForceUpdate();
   const [videoRef, videoElement] = useRefCallback<HTMLVideoElement>();
@@ -31,13 +48,16 @@ function App() {
   const handleTimeUpdate = () => {
     let currentTime = Big((videoElement as HTMLVideoElement).currentTime);
     const leftOver = currentTime.mod(Big(1).div(Big(FPS)));
-    console.log({ currentTime, leftOver });
     if (leftOver.toNumber() < 1 / FPS / 2) {
       currentTime = currentTime.minus(leftOver);
     } else {
       currentTime = currentTime.plus(Big(1).div(Big(FPS)).minus(leftOver));
     }
     setCurrentTime(Big(currentTime).toNumber());
+  };
+
+  const updateCurrentTime = (seconds: number) => () => {
+    video.currentTime = seconds;
   };
 
   const updateCurrentTimeWith = (seconds: number) => () => {
@@ -69,6 +89,8 @@ function App() {
     newTimeStamps.delete(timeStamp);
     setTimeStamps([...newTimeStamps]);
   };
+
+  const removeAllTimeStamps = () => setTimeStamps([]);
 
   return (
     <>
@@ -127,20 +149,32 @@ function App() {
             </button>
           </p>
           <p>
-            <button onClick={addTimeStamp}>Add Time stamp</button>
-            <ul>
-              {timeStamps.map((stamp) => (
-                <li key={stamp}>
-                  <button
-                    className="red small"
-                    onClick={removeTimeStamp(stamp)}
-                  >
-                    ×
-                  </button>{' '}
-                  {stamp.toFixed(2)}
-                </li>
-              ))}
-            </ul>
+            <button onClick={addTimeStamp}>Add Timestamp</button>
+            {!!timeStamps.length && (
+              <>
+                <ul>
+                  {timeStamps.map((stamp) => (
+                    <li key={stamp}>
+                      <button
+                        className="red small"
+                        onClick={removeTimeStamp(stamp)}
+                      >
+                        ×
+                      </button>{' '}
+                      <button
+                        onClick={updateCurrentTime(stamp)}
+                        className="blue small"
+                      >
+                        {toTime(stamp)}
+                      </button>
+                    </li>
+                  ))}
+                </ul>
+                <button className="red" onClick={removeAllTimeStamps}>
+                  Clear all Timestamps
+                </button>
+              </>
+            )}
           </p>
         </>
       )}
